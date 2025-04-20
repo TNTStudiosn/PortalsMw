873de0b94f2b3b65fe42cfb3d511266932a85a42
TNTStudios <tntstudiosn@gmail.com>
2025-04-20 00:20:33 -0600

    Initial commit

---
+++ src/client/java/com/TNTStudios/portalsmw/client/PortalsmwClient.java
@@ -0,0 +1,10 @@
+package com.TNTStudios.portalsmw.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PortalsmwClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/com/TNTStudios/portalsmw/Portalsmw.java
@@ -0,0 +1,10 @@
+package com.TNTStudios.portalsmw;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Portalsmw implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
